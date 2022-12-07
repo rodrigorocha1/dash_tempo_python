93b716805c64c538c9a34c44cd3184d63936dffe
rodrigorocha1 <silva.rodrigo31@outlook.com>
2022-12-07 00:15:03 -0300

    teste de layout responsivo

--- layouts/dashboard.py
+++ layouts/dashboard.py
@@ -3,6 +3,36 @@ from dash.dependencies import Input, Output, State
 from app import *
 import dash_bootstrap_components as dbc
 
-dashboard = dbc.Card([
+dashboard = html.Div(
+    [
+        dbc.Row([
+            dbc.Col(
+                dbc.Card([
 
-], className='class-card-city')
+                ], className='class-card-city')
+            ),
+            dbc.Col(
+                dbc.Card([
+
+                ], className='class-card-sol')
+            ),
+            dbc.Col(
+                dbc.Card([
+
+                ], className='class-intervalo-tempo')
+
+            ),
+        ]),
+        dbc.Row([
+            dbc.Col(
+                dbc.Card([
+
+                ], id='id_direcao_vento', ) ,
+            ),
+            dbc.Col(
+                id='coluna_mapa',
+            ),
+
+        ]),
+    ]
+),
--- main_page.py
+++ main_page.py
@@ -10,37 +10,32 @@ from layouts.calendario import calendario
 
 content = html.Div(id="page_content")
 
-app.layout = html.Div([
-    dbc.Row(
-        [
-            dbc.Col(
-                [
-                    html.Img(src='../assets/wind.png', className='class-img-barra-lateral', id='id_img_wind'),
-                    html.P("Tempo agora", id='id_txt_tempo_agora'),
-                    dcc.Location(id='url'),
-                    barra_lateral,
-                ]
-                , className='class-barra-lateral', width=2),
-            dbc.Col(
-                html.Div(id="page-content")
-            ),
-        ]
-    ),
-])
 
 
-@app.callback(Output('page-content', 'children'),
-              [Input('url', 'pathname')])
-def carregar_pagina(pathname):
-    if pathname == '/' or pathname == '/layouts/dashboard':
-        return dashboard
-    if pathname == '/' or pathname == '/layouts/mapa':
-        return mapa
-    if pathname == '/' or pathname == '/layouts/localizacao':
-        return localizacao
-    if pathname == '/' or pathname == '/layouts/calendario':
-        return calendario
+app.layout = html.Div(
+    [
 
+        dbc.Row(
+            [
+                dbc.Col(html.Div("One of four columns"), md=2, sm=6, style={'background': 'red'}),
+                dbc.Col(html.Div("One of four columns"), md=10, sm=6, style={'background': 'blue'}),
 
+            ]
+        ),
+    ]
+)
+# @app.callback(Output('page-content', 'children'),
+#               [Input('url', 'pathname')])
+# def carregar_pagina(pathname):
+#     if pathname == '/' or pathname == '/layouts/dashboard':
+#         return dashboard
+#     if pathname == '/' or pathname == '/layouts/mapa':
+#         return mapa
+#     if pathname == '/' or pathname == '/layouts/localizacao':
+#         return localizacao
+#     if pathname == '/' or pathname == '/layouts/calendario':
+#         return calendario
+#
+#
 if __name__ == "__main__":
     app.run_server(port=8051, debug=True)
